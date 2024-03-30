6170c8816a3af0e2cf7f0d5be448e794e327fb41
orangeappleak <appannagarikarthik.123@gmail.com>
2024-03-29 21:22:41 -0400

    qrCode, main.py changes

--- main.py
+++ main.py
@@ -6,15 +6,15 @@ from pathlib import Path
 import os
 import argparse
 from datetime import datetime
-import validators  # Import the validators package
+import validators
 
 # Load environment variables
 load_dotenv()
 
 # Environment Variables for Configuration
-QR_DIRECTORY = os.getenv('QR_CODE_DIR', 'qr_code')  # Directory for saving QR code
-FILL_COLOR = os.getenv('FILL_COLOR', 'black')  # Fill color for the QR code
-BACK_COLOR = os.getenv('BACK_COLOR', 'white')  # Background color for the QR code
+SECRET_DIRECTORY = os.getenv('QR_SECRET_DIR', 'secret_qr_code')
+SECRET_FILL_COLOR = os.getenv('SECRET_FILL_COLOR', 'black')
+SECRET_BACK_COLOR = os.getenv('SECRET_BACK_COLOR', 'white')
 
 def setup_logging():
     logging.basicConfig(
@@ -25,8 +25,7 @@ def setup_logging():
         ]
     )
 
-
-def create_directory(path: Path):
+def create_secret_directory(path: Path):
     try:
         path.mkdir(parents=True, exist_ok=True)
     except Exception as e:
@@ -40,9 +39,9 @@ def is_valid_url(url):
         logging.error(f"Invalid URL provided: {url}")
         return False
 
-def generate_qr_code(data, path, fill_color='black', back_color='white'):
+def generate_secret_qr_code(data, path, fill_color='black', back_color='white'):
     if not is_valid_url(data):
-        return  # Exit the function if the URL is not valid
+        return
 
     try:
         qr = qrcode.QRCode(version=1, box_size=10, border=5)
@@ -52,32 +51,26 @@ def generate_qr_code(data, path, fill_color='black', back_color='white'):
 
         with path.open('wb') as qr_file:
             img.save(qr_file)
-        logging.info(f"QR code successfully saved to {path}")
+        logging.info(f"Secret QR code successfully saved to {path}")
 
     except Exception as e:
-        logging.error(f"An error occurred while generating or saving the QR code: {e}")
+        logging.error(f"An error occurred while generating or saving the secret QR code: {e}")
 
 def main():
-    # Set up command-line argument parsing
-    parser = argparse.ArgumentParser(description='Generate a QR code.')
-    parser.add_argument('--url', help='The URL to encode in the QR code', default='https://github.com/orangeappleak')
+    parser = argparse.ArgumentParser(description='Generate a secret QR code.')
+    parser.add_argument('--url', help='The URL to encode in the secret QR code', default='https://example.com')
     args = parser.parse_args()
 
-    # Initial logging setup
     setup_logging()
-    
-    # Generate a timestamped filename for the QR code
-    timestamp = datetime.now().strftime('%Y%m%d%H%M%S')
-    qr_filename = f"QRCode_{timestamp}.png"
 
-    # Create the full path for the QR code file
-    qr_code_full_path = Path.cwd() / QR_DIRECTORY / qr_filename
+    timestamp = datetime.now().strftime('%Y%m%d%H%M%S')
+    qr_filename = f"SecretQR_{timestamp}.png"
+
+    qr_code_full_path = Path.cwd() / SECRET_DIRECTORY / qr_filename
     
-    # Ensure the QR code directory exists
-    create_directory(Path.cwd() / QR_DIRECTORY)
+    create_secret_directory(Path.cwd() / SECRET_DIRECTORY)
     
-    # Generate and save the QR code
-    generate_qr_code(args.url, qr_code_full_path, FILL_COLOR, BACK_COLOR)
+    generate_secret_qr_code(args.url, qr_code_full_path, SECRET_FILL_COLOR, SECRET_BACK_COLOR)
 
 if __name__ == "__main__":
     main()
